0a83a893c9a7c3caa416d08cf996bebdf85150da
Chris Oliver <excid3@gmail.com>
2024-11-21 15:04:32 -0600

    Refactor Jumpstart Mailer into the config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.smtp_settings = Jumpstart::Mailer.new(Jumpstart.config).settings
+  config.action_mailer.smtp_settings = Jumpstart.config.smtp_settings
 end
--- lib/jumpstart/lib/jumpstart/configuration.rb
+++ lib/jumpstart/lib/jumpstart/configuration.rb
@@ -131,6 +131,80 @@ module Jumpstart
       end
     end
 
+    module Mailer
+      def smtp_settings
+        case email_provider
+        when "mailgun"
+          {
+            address: "smtp.mailgun.org",
+            user_name: get_credential(:mailgun, :username),
+            password: get_credential(:mailgun, :password)
+          }.merge(shared_settings)
+        when "mailjet"
+          {
+            address: "in.mailjet.com",
+            user_name: get_credential(:mailjet, :username),
+            password: get_credential(:mailjet, :password)
+          }.merge(shared_settings)
+        when "mandrill"
+          {
+            address: "smtp.mandrillapp.com",
+            user_name: get_credential(:mandrill, :username),
+            password: get_credential(:mandrill, :password)
+          }.merge(shared_settings)
+        when "ohmysmtp"
+          {
+            address: "smtp.ohmysmtp.com",
+            user_name: get_credential(:ohmysmtp, :username),
+            password: get_credential(:ohmysmtp, :password)
+          }.merge(shared_settings)
+        when "postmark"
+          {
+            address: "smtp.postmarkapp.com",
+            user_name: get_credential(:postmark, :username),
+            password: get_credential(:postmark, :password)
+          }.merge(shared_settings)
+        when "sendgrid"
+          {
+            address: "smtp.sendgrid.net",
+            domain: get_credential(:sendgrid, :domain),
+            user_name: get_credential(:sendgrid, :username),
+            password: get_credential(:sendgrid, :password)
+          }.merge(shared_settings)
+        when "sendinblue"
+          shared_settings.merge({
+            address: "smtp-relay.sendinblue.com",
+            authentication: "login",
+            user_name: get_credential(:sendinblue, :username),
+            password: get_credential(:sendinblue, :password)
+          })
+        when "ses"
+          {
+            address: get_credential(:ses, :address),
+            user_name: get_credential(:ses, :username),
+            password: get_credential(:ses, :password)
+          }.merge(shared_settings)
+        when "sparkpost"
+          {
+            address: "smtp.sparkpostmail.com",
+            user_name: "SMTP_Injection",
+            password: get_credential(:sparkpost, :password)
+          }.merge(shared_settings)
+        else
+          {}
+        end
+      end
+
+      def shared_smtp_settings
+        {
+          port: 587,
+          authentication: :plain,
+          enable_starttls_auto: true,
+          domain: domain
+        }
+      end
+    end
+
     module Payable
       attr_writer :payment_processors
 
@@ -152,6 +226,7 @@ module Jumpstart
     end
 
     include Integratable
+    include Mailer
     include Payable
 
     # Attributes
@@ -296,6 +371,11 @@ module Jumpstart
     def persisted? = false
 
     private
+
+    # Search through credentials env scoped, then unscoped
+    def get_credential(*)
+      Rails.application.credentials.dig(Rails.env.to_sym, *) || Rails.application.credentials.dig(*)
+    end
 
     def procfile_content(dev: false)
       content = {web: "bundle exec rails s"}
