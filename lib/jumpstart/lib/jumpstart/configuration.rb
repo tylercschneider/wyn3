de8ec04509ba14b7c2948cf9c12ab021294558d6
Chris Oliver <excid3@gmail.com>
2024-11-21 15:11:20 -0600

    Update method name

--- lib/jumpstart/lib/jumpstart/configuration.rb
+++ lib/jumpstart/lib/jumpstart/configuration.rb
@@ -139,40 +139,40 @@ module Jumpstart
             address: "smtp.mailgun.org",
             user_name: get_credential(:mailgun, :username),
             password: get_credential(:mailgun, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "mailjet"
           {
             address: "in.mailjet.com",
             user_name: get_credential(:mailjet, :username),
             password: get_credential(:mailjet, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "mandrill"
           {
             address: "smtp.mandrillapp.com",
             user_name: get_credential(:mandrill, :username),
             password: get_credential(:mandrill, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "ohmysmtp"
           {
             address: "smtp.ohmysmtp.com",
             user_name: get_credential(:ohmysmtp, :username),
             password: get_credential(:ohmysmtp, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "postmark"
           {
             address: "smtp.postmarkapp.com",
             user_name: get_credential(:postmark, :username),
             password: get_credential(:postmark, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "sendgrid"
           {
             address: "smtp.sendgrid.net",
             domain: get_credential(:sendgrid, :domain),
             user_name: get_credential(:sendgrid, :username),
             password: get_credential(:sendgrid, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "sendinblue"
-          shared_settings.merge({
+          shared_smtp_settings.merge({
             address: "smtp-relay.sendinblue.com",
             authentication: "login",
             user_name: get_credential(:sendinblue, :username),
@@ -183,13 +183,13 @@ module Jumpstart
             address: get_credential(:ses, :address),
             user_name: get_credential(:ses, :username),
             password: get_credential(:ses, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         when "sparkpost"
           {
             address: "smtp.sparkpostmail.com",
             user_name: "SMTP_Injection",
             password: get_credential(:sparkpost, :password)
-          }.merge(shared_settings)
+          }.merge(shared_smtp_settings)
         else
           {}
         end
