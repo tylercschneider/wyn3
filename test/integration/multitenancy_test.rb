a69aa8adcdf3c788716382e51a39ad6bb9e790d7
Chris Oliver <excid3@gmail.com>
2024-12-27 20:52:20 -0600

    Split account menu from user menu (#1315)
    ---------
    
    Co-authored-by: Collin Jilbert <cjilbert504@gmail.com>

--- test/integration/multitenancy_test.rb
+++ test/integration/multitenancy_test.rb
@@ -10,48 +10,48 @@ class Jumpstart::MultitenancyTest < ActionDispatch::IntegrationTest
   test "domain multitenancy" do
     Jumpstart::Multitenancy.stub :selected, ["subdomain"] do
       get user_root_path
-      assert_select "a", text: @user.name
+      assert_select ".account-menu span", text: @user.name
 
       host! @account.domain
       sign_in @user
 
       get user_root_path
-      assert_select "a", text: @account.name
+      assert_select ".account-menu span", text: @account.name
     end
   end
 
   test "subdomain multitenancy" do
     Jumpstart::Multitenancy.stub :selected, ["subdomain"] do
       get user_root_path
-      assert_select "a", text: @user.name
+      assert_select ".account-menu span", text: @user.name
 
       host! "#{@account.subdomain}.example.com"
       sign_in @user
 
       get user_root_path
-      assert_select "a", text: @account.name
+      assert_select ".account-menu span", text: @account.name
     end
   end
 
   test "script path multitenancy" do
     Jumpstart::Multitenancy.stub :selected, ["path"] do
       get "/"
-      assert_select "a", text: @user.name
+      assert_select ".account-menu span", text: @user.name
 
       get "/#{@account.id}/"
-      assert_select "a", text: @account.name
+      assert_select ".account-menu span", text: @account.name
     end
   end
 
   test "session multitenancy" do
     Jumpstart::Multitenancy.stub :selected, [] do
       get user_root_path
-      assert_select "a", text: @user.name
+      assert_select ".account-menu span", text: @user.name
 
       switch_account(@account)
 
       get user_root_path
-      assert_select "a", text: @account.name
+      assert_select ".account-menu span", text: @account.name
     end
   end
 end
