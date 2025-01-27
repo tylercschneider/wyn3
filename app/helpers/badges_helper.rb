fb191020a55736ab6983c1dd0df0c4936efba576
Chris Oliver <excid3@gmail.com>
2025-01-27 15:39:59 -0600

    Lint

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -5,7 +5,7 @@ module BadgesHelper
   #   Active
   # <% end %>
   def badge(text = nil, options = {}, &block)
-    text, options = nil, (text || {}) if block
+    text, options = nil, text || {} if block
     base = options.delete(:base) || "rounded-sm py-0.5 px-2 text-xs inline-block font-semibold leading-normal mr-2"
     color = options.delete(:color) || "bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200"
     options[:class] = Array.wrap(options[:class]) + [base, color]
