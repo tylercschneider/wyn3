c1a4a5560782cd6af176f40a9bdf02b5ae58bd5c
Chris Oliver <excid3@gmail.com>
2025-02-03 08:53:17 -0600

    Improve logic for creating default account by using the JSP config

--- app/models/user/accounts.rb
+++ app/models/user/accounts.rb
@@ -22,7 +22,10 @@ module User::Accounts
     # Invited users don't have a name immediately, so we will run this method twice for them
     # once on create where no name is present and again on accepting the invitation
     return if name.blank?
-    return accounts.first if accounts.any?
+
+    if (account = accounts.where(personal: Jumpstart.config.personal_accounts?).first)
+      return account
+    end
 
     account = accounts.new(owner: self, name: name, personal: Jumpstart.config.personal_accounts?)
     account.account_users.new(user: self, admin: true)
