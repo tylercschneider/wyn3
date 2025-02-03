d36ae3a765a803a3ff6bb6f0b744d57d4b9425d6
Chris Oliver <excid3@gmail.com>
2025-02-03 10:57:46 -0600

    Skip creating default account on API request if an account is provided

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::UsersController < Api::BaseController
 
     # If registering with an account, add the AccountUser with admin role
     if Jumpstart.config.register_with_account?
+      user.skip_default_account = true
       account = user.owned_accounts.first_or_initialize
       account.account_users.new(user: user, admin: true)
     end
