b540b717bf0bbad507b7d3fd55cda41914507675
Chris Oliver <excid3@gmail.com>
2025-03-03 08:12:19 -0600

    Update importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "@rails/activestorage", to: "activestorage.esm.js"
-pin "trix" # @2.1.11
+pin "trix" # @2.1.12
 
 # Vendor libraries
 pin "@hotwired/hotwire-native-bridge", to: "@hotwired--hotwire-native-bridge.js" # @1.0.0
@@ -20,7 +20,7 @@ pin "clipboard" # @2.0.11
 pin "local-time", to: "local-time.es2017-esm.js"
 pin "tailwindcss-stimulus-components" # @6.1.3
 pin "tributejs" # @5.1.3
-pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.6.12
-pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.6.8
-pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.8
-pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.8
+pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.6.13
+pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.6.9
+pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.9
+pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.9
