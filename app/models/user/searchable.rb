7246cf677d9616e5b752f9fcc8521daad1f5ace1
Chris Oliver <excid3@gmail.com>
2025-06-25 15:59:30 -0500

    Use virtual column for name now that Rails supports it with SQLite. #1419

--- app/models/user/searchable.rb
+++ app/models/user/searchable.rb
@@ -6,17 +6,7 @@ module User::Searchable
 
   class_methods do
     def search(query)
-      case connection.adapter_name
-      when "SQLite"
-        first_name, last_name = query.split(" ", 2)
-        if last_name.present?
-          where(arel_table[:first_name].matches("%#{sanitize_sql_like(first_name)}%")).where(arel_table[:last_name].matches("%#{sanitize_sql_like(last_name)}%"))
-        else
-          where(arel_table[:first_name].matches("%#{sanitize_sql_like(query)}%")).or(where(arel_table[:last_name].matches("%#{sanitize_sql_like(query)}%")))
-        end
-      else
-        where(arel_table[:name].matches("%#{sanitize_sql_like(query)}%"))
-      end
+      where(arel_table[:name].matches("%#{sanitize_sql_like(query)}%"))
     end
   end
 end
--- db/migrate/20230810152614_add_name_virtual_column_to_users.rb
+++ db/migrate/20230810152614_add_name_virtual_column_to_users.rb
@@ -1,11 +1,9 @@
 class AddNameVirtualColumnToUsers < ActiveRecord::Migration[7.0]
-  # Rails doesn't support virtual columns for SQLite yet
-
   def change
     case connection.adapter_name
     when "Trilogy", "Mysql2"
       add_column :users, :name, :virtual, type: :string, as: "CONCAT_WS(' ', first_name, last_name)", stored: true
-    when "PostgreSQL"
+    when "PostgreSQL", "SQLite"
       add_column :users, :name, :virtual, type: :string, as: "first_name || ' ' || coalesce(last_name, '')", stored: true
     end
   end
