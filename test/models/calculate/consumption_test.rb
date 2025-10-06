df5b61f457b1a1707470549f2c5563711e3746d5
tylercschneider <tylercschneider@gmail.com>
2025-10-06 13:36:10 -0400

    change setup variable for clarity

--- test/models/calculate/consumption_test.rb
+++ test/models/calculate/consumption_test.rb
@@ -7,14 +7,14 @@ class Calculate::ConsumptionTest < ActiveSupport::TestCase
   end
 
   def test_consumed_by_date
-    expire_after_date = Date.new(2024, 1, 11)
-    assert_equal expire_after_date, Calculate::Consumption.consumed_by_date(start_date: Date.new(2024, 1, 1), num_of_days: 10)
+    consumed_by_date = Date.new(2024, 1, 11)
+    assert_equal consumed_by_date, Calculate::Consumption.consumed_by_date(start_date: Date.new(2024, 1, 1), num_of_days: 10)
   end
 
   def test_consumed_by_date_no_start_date_specified
     travel_to Time.zone.parse("2025-10-02 09:00:00") do
-      expire_after_date = Date.new(2025, 10, 12)
-      assert_equal expire_after_date, Calculate::Consumption.consumed_by_date(num_of_days: 10)
+      consumed_by_date = Date.new(2025, 10, 12)
+      assert_equal consumed_by_date, Calculate::Consumption.consumed_by_date(num_of_days: 10)
     end
   end
 end
