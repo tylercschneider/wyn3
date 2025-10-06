c21cafa509fca1dec37da4e8e23f0fdb52d63673
tylercschneider <tylercschneider@gmail.com>
2025-10-06 13:27:55 -0400

    random small changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_18_192532) do
+ActiveRecord::Schema[8.1].define(version: 2025_06_15_033635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "account_invitations", force: :cascade do |t|
     t.bigint "account_id", null: false
-    t.bigint "invited_by_id"
-    t.string "token", null: false
-    t.string "name", null: false
-    t.string "email", null: false
-    t.jsonb "roles", default: {}, null: false
     t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.bigint "invited_by_id"
+    t.string "name", null: false
+    t.jsonb "roles", default: {}, null: false
+    t.string "token", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id", "email"], name: "index_account_invitations_on_account_id_and_email", unique: true
     t.index ["invited_by_id"], name: "index_account_invitations_on_invited_by_id"
@@ -30,62 +30,62 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_18_192532) do
 
   create_table "account_users", force: :cascade do |t|
     t.bigint "account_id"
-    t.bigint "user_id"
-    t.jsonb "roles", default: {}, null: false
     t.datetime "created_at", null: false
+    t.jsonb "roles", default: {}, null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["account_id", "user_id"], name: "index_account_users_on_account_id_and_user_id", unique: true
   end
 
   create_table "accounts", force: :cascade do |t|
+    t.integer "account_users_count", default: 0
+    t.string "billing_email"
+    t.datetime "created_at", null: false
+    t.string "domain"
+    t.text "extra_billing_info"
     t.string "name", null: false
     t.bigint "owner_id"
     t.boolean "personal", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "extra_billing_info"
-    t.string "domain"
     t.string "subdomain"
-    t.string "billing_email"
-    t.integer "account_users_count", default: 0
+    t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_accounts_on_owner_id"
   end
 
   create_table "action_text_embeds", force: :cascade do |t|
-    t.string "url"
-    t.jsonb "fields"
     t.datetime "created_at", null: false
+    t.jsonb "fields"
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "action_text_rich_texts", force: :cascade do |t|
-    t.string "name", null: false
     t.text "body"
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.datetime "updated_at", precision: nil, null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", precision: nil, null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
@@ -97,71 +97,71 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_18_192532) do
   end
 
   create_table "announcements", force: :cascade do |t|
-    t.string "kind"
-    t.string "title"
-    t.datetime "published_at", precision: nil
     t.datetime "created_at", null: false
+    t.string "kind"
+    t.datetime "published_at", precision: nil
+    t.string "title"
     t.datetime "updated_at", null: false
   end
 
   create_table "api_tokens", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "token"
-    t.string "name"
-    t.jsonb "metadata"
-    t.boolean "transient", default: false
-    t.datetime "last_used_at", precision: nil
-    t.datetime "expires_at", precision: nil
     t.datetime "created_at", null: false
+    t.datetime "expires_at", precision: nil
+    t.datetime "last_used_at", precision: nil
+    t.jsonb "metadata"
+    t.string "name"
+    t.string "token"
+    t.boolean "transient", default: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["token"], name: "index_api_tokens_on_token", unique: true
     t.index ["user_id"], name: "index_api_tokens_on_user_id"
   end
 
   create_table "connected_accounts", force: :cascade do |t|
-    t.bigint "owner_id"
-    t.string "provider"
-    t.string "uid"
-    t.string "refresh_token"
-    t.datetime "expires_at", precision: nil
-    t.jsonb "auth"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
     t.string "access_token"
     t.string "access_token_secret"
+    t.jsonb "auth"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "expires_at", precision: nil
+    t.bigint "owner_id"
     t.string "owner_type"
+    t.string "provider"
+    t.string "refresh_token"
+    t.string "uid"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["owner_id", "owner_type"], name: "index_connected_accounts_on_owner_id_and_owner_type"
   end
 
   create_table "inbound_webhooks", force: :cascade do |t|
-    t.integer "status", default: 0, null: false
     t.text "body"
     t.datetime "created_at", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "noticed_events", force: :cascade do |t|
     t.bigint "account_id"
-    t.string "type"
-    t.string "record_type"
-    t.bigint "record_id"
-    t.jsonb "params"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer "notifications_count"
+    t.jsonb "params"
+    t.bigint "record_id"
+    t.string "record_type"
+    t.string "type"
+    t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_noticed_events_on_account_id"
     t.index ["record_type", "record_id"], name: "index_noticed_events_on_record"
   end
 
   create_table "noticed_notifications", force: :cascade do |t|
     t.bigint "account_id"
-    t.string "type"
-    t.bigint "event_id", null: false
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.datetime "read_at", precision: nil
-    t.datetime "seen_at", precision: nil
     t.datetime "created_at", null: false
+    t.bigint "event_id", null: false
+    t.datetime "read_at", precision: nil
+    t.bigint "recipient_id", null: false
+    t.string "recipient_type", null: false
+    t.datetime "seen_at", precision: nil
+    t.string "type"
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_noticed_notifications_on_account_id"
     t.index ["event_id"], name: "index_noticed_notifications_on_event_id"
@@ -169,192 +169,192 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_18_192532) do
   end
 
   create_table "notification_tokens", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "token", null: false
-    t.string "platform", null: false
     t.datetime "created_at", null: false
+    t.string "platform", null: false
+    t.string "token", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_notification_tokens_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
     t.bigint "account_id", null: false
-    t.string "recipient_type", null: false
-    t.bigint "recipient_id", null: false
-    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "interacted_at", precision: nil
     t.jsonb "params"
     t.datetime "read_at", precision: nil
-    t.datetime "created_at", null: false
+    t.bigint "recipient_id", null: false
+    t.string "recipient_type", null: false
+    t.string "type"
     t.datetime "updated_at", null: false
-    t.datetime "interacted_at", precision: nil
     t.index ["account_id"], name: "index_notifications_on_account_id"
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient_type_and_recipient_id"
   end
 
   create_table "pay_charges", force: :cascade do |t|
-    t.string "processor_id", null: false
     t.integer "amount", null: false
     t.integer "amount_refunded"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.jsonb "data"
     t.integer "application_fee_amount"
+    t.datetime "created_at", precision: nil, null: false
     t.string "currency"
-    t.jsonb "metadata"
-    t.integer "subscription_id"
     t.bigint "customer_id"
-    t.string "stripe_account"
-    t.string "type"
+    t.jsonb "data"
+    t.jsonb "metadata"
     t.jsonb "object"
+    t.string "processor_id", null: false
+    t.string "stripe_account"
+    t.integer "subscription_id"
+    t.string "type"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["customer_id", "processor_id"], name: "index_pay_charges_on_customer_id_and_processor_id", unique: true
   end
 
   create_table "pay_customers", force: :cascade do |t|
-    t.string "owner_type"
+    t.datetime "created_at", null: false
+    t.jsonb "data"
+    t.boolean "default"
+    t.datetime "deleted_at", precision: nil
+    t.jsonb "object"
     t.bigint "owner_id"
+    t.string "owner_type"
     t.string "processor"
     t.string "processor_id"
-    t.boolean "default"
-    t.jsonb "data"
-    t.datetime "deleted_at", precision: nil
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "stripe_account"
     t.string "type"
-    t.jsonb "object"
+    t.datetime "updated_at", null: false
     t.index ["owner_type", "owner_id", "deleted_at"], name: "customer_owner_processor_index"
     t.index ["processor", "processor_id"], name: "index_pay_customers_on_processor_and_processor_id"
   end
 
   create_table "pay_merchants", force: :cascade do |t|
-    t.string "owner_type"
+    t.datetime "created_at", null: false
+    t.jsonb "data"
+    t.boolean "default"
     t.bigint "owner_id"
+    t.string "owner_type"
     t.string "processor"
     t.string "processor_id"
-    t.boolean "default"
-    t.jsonb "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "type"
+    t.datetime "updated_at", null: false
     t.index ["owner_type", "owner_id", "processor"], name: "index_pay_merchants_on_owner_type_and_owner_id_and_processor"
   end
 
   create_table "pay_payment_methods", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.bigint "customer_id"
-    t.string "processor_id"
+    t.jsonb "data"
     t.boolean "default"
     t.string "payment_method_type"
-    t.jsonb "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "processor_id"
     t.string "stripe_account"
     t.string "type"
+    t.datetime "updated_at", null: false
     t.index ["customer_id", "processor_id"], name: "index_pay_payment_methods_on_customer_id_and_processor_id", unique: true
   end
 
   create_table "pay_subscriptions", id: :serial, force: :cascade do |t|
+    t.decimal "application_fee_percent", precision: 8, scale: 2
+    t.datetime "created_at", precision: nil
+    t.datetime "current_period_end"
+    t.datetime "current_period_start"
+    t.bigint "customer_id"
+    t.jsonb "data"
+    t.datetime "ends_at", precision: nil
+    t.jsonb "metadata"
+    t.boolean "metered"
     t.string "name", null: false
+    t.jsonb "object"
+    t.string "pause_behavior"
+    t.datetime "pause_resumes_at"
+    t.datetime "pause_starts_at"
+    t.string "payment_method_id"
     t.string "processor_id", null: false
     t.string "processor_plan", null: false
     t.integer "quantity", default: 1, null: false
-    t.datetime "trial_ends_at", precision: nil
-    t.datetime "ends_at", precision: nil
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
     t.string "status"
-    t.jsonb "data"
-    t.decimal "application_fee_percent", precision: 8, scale: 2
-    t.jsonb "metadata"
-    t.bigint "customer_id"
-    t.datetime "current_period_start"
-    t.datetime "current_period_end"
-    t.boolean "metered"
-    t.string "pause_behavior"
-    t.datetime "pause_starts_at"
-    t.datetime "pause_resumes_at"
-    t.string "payment_method_id"
     t.string "stripe_account"
+    t.datetime "trial_ends_at", precision: nil
     t.string "type"
-    t.jsonb "object"
+    t.datetime "updated_at", precision: nil
     t.index ["customer_id", "processor_id"], name: "index_pay_subscriptions_on_customer_id_and_processor_id", unique: true
     t.index ["metered"], name: "index_pay_subscriptions_on_metered"
     t.index ["pause_starts_at"], name: "index_pay_subscriptions_on_pause_starts_at"
   end
 
   create_table "pay_webhooks", force: :cascade do |t|
-    t.string "processor"
-    t.string "event_type"
-    t.jsonb "event"
     t.datetime "created_at", null: false
+    t.jsonb "event"
+    t.string "event_type"
+    t.string "processor"
     t.datetime "updated_at", null: false
   end
 
   create_table "plans", force: :cascade do |t|
-    t.string "name", null: false
     t.integer "amount", default: 0, null: false
-    t.string "interval", null: false
-    t.jsonb "details"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.integer "trial_period_days", default: 0
-    t.boolean "hidden"
-    t.string "currency"
-    t.integer "interval_count", default: 1
-    t.string "description"
-    t.string "unit_label"
-    t.boolean "charge_per_unit"
-    t.string "stripe_id"
     t.string "braintree_id"
+    t.boolean "charge_per_unit"
+    t.string "contact_url"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "currency"
+    t.string "description"
+    t.jsonb "details"
+    t.string "fake_processor_id"
+    t.boolean "hidden"
+    t.string "interval", null: false
+    t.integer "interval_count", default: 1
+    t.string "lemon_squeezy_id"
+    t.string "name", null: false
     t.string "paddle_billing_id"
     t.string "paddle_classic_id"
-    t.string "lemon_squeezy_id"
-    t.string "fake_processor_id"
-    t.string "contact_url"
+    t.string "stripe_id"
+    t.integer "trial_period_days", default: 0
+    t.string "unit_label"
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
     t.bigint "account_id", null: false
+    t.text "body"
     t.datetime "created_at", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: nil
-    t.datetime "remember_created_at", precision: nil
+    t.datetime "accepted_privacy_at", precision: nil
+    t.datetime "accepted_terms_at", precision: nil
+    t.boolean "admin"
+    t.datetime "announcements_read_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "confirmation_token"
     t.datetime "confirmed_at", precision: nil
-    t.datetime "confirmation_sent_at", precision: nil
-    t.string "unconfirmed_email"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "time_zone"
-    t.datetime "accepted_terms_at", precision: nil
-    t.datetime "accepted_privacy_at", precision: nil
-    t.datetime "announcements_read_at", precision: nil
-    t.boolean "admin"
     t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
-    t.string "invitation_token"
-    t.datetime "invitation_created_at", precision: nil
-    t.datetime "invitation_sent_at", precision: nil
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
     t.datetime "invitation_accepted_at", precision: nil
+    t.datetime "invitation_created_at", precision: nil
     t.integer "invitation_limit"
-    t.string "invited_by_type"
-    t.bigint "invited_by_id"
+    t.datetime "invitation_sent_at", precision: nil
+    t.string "invitation_token"
     t.integer "invitations_count", default: 0
-    t.string "preferred_language"
+    t.bigint "invited_by_id"
+    t.string "invited_by_type"
+    t.string "last_name"
+    t.integer "last_otp_timestep"
+    t.virtual "name", type: :string, as: "(((first_name)::text || ' '::text) || (COALESCE(last_name, ''::character varying))::text)", stored: true
+    t.text "otp_backup_codes"
     t.boolean "otp_required_for_login"
     t.string "otp_secret"
-    t.integer "last_otp_timestep"
-    t.text "otp_backup_codes"
     t.jsonb "preferences"
-    t.virtual "name", type: :string, as: "(((first_name)::text || ' '::text) || (COALESCE(last_name, ''::character varying))::text)", stored: true
+    t.string "preferred_language"
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "reset_password_sent_at", precision: nil
+    t.string "reset_password_token"
+    t.string "time_zone"
+    t.string "unconfirmed_email"
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
